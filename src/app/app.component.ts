610946e48d88f0820b7c058a394d7e0a48e47d88
TheNguyenNgoc <thenguyenngochn@gmail.com>
2022-05-14 23:13:19 +0700

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'general-pass';
+  public numberGen = 0;
+  includeLetters = false;
+
+  click() {
+    console.log('aaaaa');
+  }
+
+  onChangeIncludeLetters() {
+    this.includeLetters = !this.includeLetters;
+    console.log(this.includeLetters);
+  }
 }
